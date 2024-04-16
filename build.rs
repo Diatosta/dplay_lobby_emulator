65b67dc1145bf9749f403ecb054580e5724ca829
Rafael Monteiro <diatosta@hotmail.com>
2024-04-16 02:50:18 +0100

    Added icon to built .exe

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+use winres::WindowsResource;
+
 fn main() {
     slint_build::compile("ui/appwindow.slint").unwrap();
+
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            .set_icon("ui/icon.ico")
+            .compile().unwrap();
+    }
 }
