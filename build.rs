f7bd9763f9a68db83475d55ef65a92d43a5fb58a
Rafael Monteiro <diatosta@hotmail.com>
2024-04-21 21:13:00 +0100

    Fix git workflow, attempt 5

--- build.rs
+++ build.rs
@@ -6,8 +6,12 @@ fn main() {
     slint_build::compile("ui/appwindow.slint").unwrap();
 
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        WindowsResource::new()
+        let result = WindowsResource::new()
             .set_icon("ui/icon.ico")
-            .compile().unwrap();
+            .compile();
+
+        if let Err(e) = result {
+            eprintln!("Failed to compile resources: {}", e);
+        }
     }
 }
